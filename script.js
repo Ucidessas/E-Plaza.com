3cd1c4e604cfd7fef909fdaf8907e108b8e8bde8
edward quevedo <edwardquevedop@gmail.com>
2025-01-04 18:11:25 -0500

    subida  de productos prueba 3 junior

--- script.js
+++ script.js
@@ -465,7 +465,7 @@ function displayProducts1(data) {
     detailContainer.innerHTML = `
   
        <div class="virtual-tour">
-                  <h3>"${property.name}"</h3>
+                  <h3>${property.name}</h3>
                   <img src="${property.image}" alt="Foto portada propiedad">
               </div>
   
